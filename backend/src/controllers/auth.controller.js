e5c882c467e43fb395ca1730e16ebf6b1f369693
AyushRay06 <rayayush47@gmail.com>
2025-03-14 20:32:48 +0530

    signup-controll

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,9 +1,51 @@
-export const signup = (req, res) => {
-  res.json({ message: "Fuck off mate" })
+import { generateToken } from "../lib/jwt.js"
+import User from "../models/user.modal.js"
+import bcrypt from "bcryptjs"
+
+export const signup = async (req, res) => {
+  const { email, fullName, password } = req.body
+  try {
+    if (password.legth < 6) {
+      return res
+        .status(400)
+        .json({ message: "Password must be atleast 6 characters" })
+    }
+
+    const existingUser = await User.findOne({ email })
+    if (existingUser) {
+      res.status(400).json({ message: "User already exist" })
+      return
+    }
+
+    const salt = await bcrypt.genSalt(10)
+    const hashedPassword = bcrypt.hash((password, salt))
+
+    //Before saving we need to hash
+    const newUser = await User.create({
+      email,
+      fullName,
+      password: hashedPassword,
+    })
+    // WHEN THE NEW USER IS CREATED WE NEED TO ASSIGN OR CREATE A JWT TOKEN FOR HIM/HER.
+    if (newUser) {
+      generateToken(newUser._id, res)
+      await newUser.save()
+      re.status(200).json({
+        _id: newUser._id,
+        fullName: newUser.fullName,
+        email: newUser.email,
+        profilePic: newUser.profilePic,
+      })
+    } else {
+      res.status(400).json({ message: "Inavalid user details" })
+    }
+  } catch (error) {
+    console.log(`Error in signup controller!!!, ${error}`)
+  }
 }
 
 export const login = (req, res) => {
-  res.send("Hello")
+  
 }
 
 export const logout = (req, res) => {
--- backend/src/index.js
+++ backend/src/index.js
@@ -1,10 +1,12 @@
-import express from "express"
+import express, { json } from "express"
 import authRoutes from "./routes/auth.route.js"
 import dotenv from "dotenv"
 import { ConnectDB } from "./lib/db.js"
 
 dotenv.config()
 const app = express()
+// allow to extract jsonm data from the body
+app.use(express.json())
 app.use("api/auth", authRoutes)
 
 const PORT = process.env.PORT
